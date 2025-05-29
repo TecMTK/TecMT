535c93084ef0d9f67a967243c10f954e4cd05864
TecMTK <tec.mt1022@gmail.com>
2025-05-29 13:26:47 -0300

    TecMT

---
+++ script.js
@@ -0,0 +1,16 @@
+function faleColaborador() {
+  // Com mensagem automática
+  const mensagem = "Olá Matheus! Gostaria de saber mais sobre seus serviços de tecnologia.";
+  const link = "https://wa.me/558491895728?text=" + encodeURIComponent(mensagem);
+  window.location.href = link;
+}
+
+function faleColaborador() {
+  // Com mensagem automática
+  const mensagem = "Olá Felipe! Gostaria de saber mais sobre seus serviços de tecnologia.";
+  const link = "https://wa.me/5584988081473?text=" + encodeURIComponent(mensagem);
+  window.location.href = link;
+}
+
+
+
